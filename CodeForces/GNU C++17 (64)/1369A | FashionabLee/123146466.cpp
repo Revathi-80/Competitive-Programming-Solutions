6a3aca8722f3e5bf9240200b4b2b06444582a00c
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:05:42 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1369A | FashionabLee/123146466.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+      int t;
+      cin>>t;
+      while(t--){
+            long long int n;
+            cin>>n;
+            if(n%4==0) cout<<"YES"<<endl;
+            else cout<<"NO"<<endl;
+      }
+      return 0;
+}
