462e17316bf3890ee313e30e767f88b0ca8ee715
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:00:47 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1589A | Mathematical Addition/135559933.cpp
@@ -0,0 +1,14 @@
+ 
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main(){
+ ll t;
+ cin>>t;
+ while(t--){
+ll u,v;
+cin>>u>>v;
+cout<<-(u*u)<<" "<<v*v<<endl;
+}
+return 0;
+}
