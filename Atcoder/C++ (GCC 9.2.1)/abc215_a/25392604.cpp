f8005086d89c6e2a092a62898a035cc72ed431b9
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 19:51:50 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc215_a/25392604.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  string s;
+  cin>>s;
+  string str="Hello,World!";
+  //int res= strcmp(s,str);
+  if(s==str) cout<<"AC"<<endl;
+  else cout<<"WA"<<endl;
+  return 0;
+}
