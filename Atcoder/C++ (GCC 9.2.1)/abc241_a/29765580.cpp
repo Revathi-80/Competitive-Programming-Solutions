390fed4e1a941726daf5f515f896d22faac40c52
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 19:51:43 +0530

    Committing files

---
+++ Atcoder/C++ (GCC 9.2.1)/abc241_a/29765580.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+	ll arr[10];
+	ll next=0;
+	for(int i=0;i<10;i++) {
+		cin>>arr[i];
+		
+	}
+	next=arr[next];
+	next=arr[next];
+	next=arr[next];
+	cout<<next<<endl;
+}
+ 
+int main()
+{
+   //int t;
+   //cin>>t;
+  
+   // while(t){
+   solve();
+   //t--;
+    //}
+    return 0;
+}
