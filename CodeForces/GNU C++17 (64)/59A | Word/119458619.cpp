7cecbf6c2e08e8d025068e4fa0ce6cb515b1a545
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:06:32 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/59A | Word/119458619.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ string str;
+cin>>str;
+int l=str.length();
+int count1=0,count2=0;
+for(int i=0;i<l;i++){
+    if(str[i]>='a' && str[i]<='z'){
+    count1++;
+    }else
+    count2++;
+}
+//cout<<count1<<count2<<'\n';
+if(count1>=count2){
+    transform(str.begin(),str.end(),str.begin(),::tolower);
+    cout<<str<<'\n';
+}
+else{
+    transform(str.begin(),str.end(),str.begin(), ::toupper);
+    cout<<str<<'\n';
+}
+ return 0;
+}
