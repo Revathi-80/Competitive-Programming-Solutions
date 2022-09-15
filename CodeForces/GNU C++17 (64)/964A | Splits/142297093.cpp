bb2f9bd0a1721561af1761e01f58d86d1766311a
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 19:59:20 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/964A | Splits/142297093.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define sz(x) ((int)(x).size())
+const long long  mod = 1000000007;
+const int x=1e7;
+void solve(){
+ int n;
+ cin>>n;
+ cout<<n/2 +1<<endl;
+}
+int main(){
+// int t;
+// cin>>t;
+ ///while(t){
+ // t--;
+  solve();
+// }
+return 0;
+}
