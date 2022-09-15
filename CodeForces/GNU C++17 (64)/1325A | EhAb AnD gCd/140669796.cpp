ea410eb18538c1051eb58088747efaa9a1ffa27a
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:00:18 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1325A | EhAb AnD gCd/140669796.cpp
@@ -0,0 +1,18 @@
+ 
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+int x;
+cin>>x;
+cout<<"1 "<<x-1<<endl;
+ 
+}
+int main(){
+int t;
+cin>>t;
+while(t){
+ solve();
+ t--;
+}
+return 0;
+}
