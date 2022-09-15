d99357b8cd6ac51a880c7b601280d16ab2b9feab
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:04:36 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/268A | Games/127562978.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+   int n,cnt=0;
+   cin>>n;
+   int a[n],b[n];
+   for(int i=0;i<n;i++){
+       cin>>a[i]>>b[i];
+   }
+   for(int i=0;i<n;i++){
+       for(int j=0;j<n;j++){
+           if(a[i]==b[j]) cnt++;
+       }
+   }
+   cout<<cnt;
+ return 0;
+}
