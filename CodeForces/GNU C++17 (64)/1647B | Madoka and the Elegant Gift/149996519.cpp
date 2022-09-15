52bdba6bf9c01554b97c57a57d8fc3e7e7e8554a
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 19:56:51 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1647B | Madoka and the Elegant Gift/149996519.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve(){
+ int n,m;
+  cin>>n>>m;
+  vector<vector<int>>a(n,vector<int>(m));
+  for(int i=0;i<n;i++){
+  string s;
+  cin>>s;
+  for(int j=0;j<m;j++){
+    a[i][j]=s[j]-'0';
+  }
+  }
+  for(int i=0;i<n-1;i++){
+    for(int j=0;j<m-1;j++){
+      int sum=a[i][j]+a[i][j+1]+a[i+1][j]+a[i+1][j+1];
+      if(sum==3){
+        cout<<"NO"<<endl;
+        return;
+      }
+    }
+  }
+  cout<<"YES"<<endl;
+ }
+int main()
+{
+   int t;
+   cin>>t;
+  
+    while(t){
+   solve();
+   t--;
+    }
+    return 0;
+}
