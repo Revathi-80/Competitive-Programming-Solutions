8cac027b8433217b909b3c3c7d7c811c13ce34c7
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 19:56:14 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1665A | GCD vs LCM/153034794.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve(){
+ ll n;
+ cin>>n;
+ 
+ cout<<n-3<<" "<<1<<" "<<1<<" "<<1<<endl;
+}
+int main()
+{
+   int t;
+   cin>>t;
+  
+    while(t){
+   solve();
+   t--;
+    }
+    return 0;
+}
+ 
