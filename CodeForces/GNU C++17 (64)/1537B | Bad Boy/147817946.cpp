cdc83f488dc89c2bd63a764db29b5cb3cd36cf9f
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 19:57:35 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1537B | Bad Boy/147817946.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve(){
+int n,m,x,y;
+cin>>n>>m>>x>>y;
+cout<<"1 1 "<<n<<" "<<m<<endl;
+}
+ 
+int main()
+{
+   int t;
+   cin>>t;
+  
+    while(t){
+   solve();
+   t--;
+    }
+    return 0;
+}
