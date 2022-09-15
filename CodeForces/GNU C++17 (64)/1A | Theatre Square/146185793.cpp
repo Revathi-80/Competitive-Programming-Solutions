766a3a5b3d732c846e1f3e6f99b2ee67d937bdfc
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 19:58:06 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1A | Theatre Square/146185793.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+ 
+void solve(){
+double n,m,a;
+cin>>n>>m>>a;
+ll ans=ceil(n/a)*ceil(m/a);
+cout<<ans<<endl;
+}
+ 
+ 
+int main()
+{
+  // int t;
+   //cin>>t;
+    //while(t){
+    solve();
+   // t--;
+   // }
+    return 0;
+}
