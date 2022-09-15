96e59f04a8c00d93d314ca5b60261a4e038214cc
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:03:56 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/1555A | PizzaForces/128131297.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+long long int t;
+ cin>>t;
+ while(t--){
+  long long int n;
+  cin>>n;
+  cout<<max(6LL,n+1)/2 *5<<endl;
+ }
+ return 0;
+}
