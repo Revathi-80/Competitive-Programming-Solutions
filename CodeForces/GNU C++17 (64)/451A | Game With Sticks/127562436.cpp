30b116b23281c4ebcc14850a617a65da50afec3b
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:04:39 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/451A | Game With Sticks/127562436.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main(){
+ int n,m;
+ cin>>n>>m;
+ int res=0;
+ int inter=min(n,m);
+if(inter%2!=0) cout<<"Akshat"<<endl;
+else cout<<"Malvika"<<endl;
+ return 0;
+}
