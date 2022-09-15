1be3aff25699e62725e315ccbcac81694888b6e6
REVATHI P <89781910+Revathi-80@users.noreply.github.com>
2022-09-15 20:06:56 +0530

    Committing files

---
+++ CodeForces/GNU C++17 (64)/467A | George and Accommodation/117964679.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+int n,p,q,count=0;
+cin>>n;
+for(int i=0;i<n;i++){
+    cin>>p>>q;
+    if(p<=(q-2)) count++;
+}
+cout<<count<<endl;
+ return 0;
+}
